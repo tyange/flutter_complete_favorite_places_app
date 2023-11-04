fb3141cd1123247aec51834cd32700b0f7db3711
tyange <usun16@gmail.com>
2023-11-04 19:49:01 +0900

    add location input

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -2,6 +2,7 @@ import "dart:io";
 
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_favorite_places_app/widgets/image_input.dart';
+import 'package:flutter_complete_favorite_places_app/widgets/location_input.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:flutter_complete_favorite_places_app/providers/user_places.dart';
@@ -62,6 +63,10 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 _selectedImage = image;
               },
             ),
+            const SizedBox(
+              height: 10,
+            ),
+            LocationInput(),
             const SizedBox(
               height: 16,
             ),
